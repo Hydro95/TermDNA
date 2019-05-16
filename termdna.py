c58a64a930c3abae9f7f1eb2046fd358c1905f54
Josh Vinge <joshvinge@outlook.com>
2019-05-16 09:00:57 -0400

    Clean up to become more awesome

---
+++ termdna.py
@@ -0,0 +1,44 @@
+import sys
+from random import choice
+from termcolor import colored
+
+DNA_LENGTH = 20
+
+if len(sys.argv) > 1:
+    DNA_LENGTH = int(sys.argv[1])
+
+CHAR_MAP = {
+    'T': {'color': 'red', 'links': 'A'},
+    'A': {'color': 'green', 'links': 'T'},
+    'C': {'color': 'yellow', 'links': 'G'},
+    'G': {'color': 'blue', 'links': 'C'}
+}
+
+
+def ch_colored(chara):
+    return colored(chara, CHAR_MAP[chara]['color'])
+
+
+def get_link(chara):
+    return CHAR_MAP[chara]['links']
+
+
+def gen_dna(length, mid=''):
+
+    top = ''
+    bot = ''
+    chars = list(CHAR_MAP.keys())
+
+    for _ in range(length):
+        rand_ch = choice(chars)
+        top += ch_colored(rand_ch)
+        bot += ch_colored(get_link(rand_ch))
+
+    return ''.join('{0}\n{1}\n{2}'.format(top, mid * length, bot))
+
+
+def main():
+    print(gen_dna(DNA_LENGTH, mid='|'))
+
+
+main()
